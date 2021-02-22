267fd9677d851cccdfc3f9b453978881ac7f951c
Luis Herrera <l.herrera.garnica@gmail.com>
2021-02-22 12:55:45 -0300

    creación del proyecto con librerías básicas sin desarrollo, pero funcional con credenciales estáticas

---
+++ src/main/java/cl/lherrera/scraper/PipappApplication.java
@@ -0,0 +1,13 @@
+package cl.lherrera.scraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipappApplication.class, args);
+	}
+
+}
